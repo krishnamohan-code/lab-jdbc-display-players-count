265da3c56c12f48b491dbb1f6f2e7075f5692eb6
krishnamohan <krishnamohan.py@gmail.com>
2020-06-22 10:25:57 +0530

    'FSJ0520A119'

--- src/model/Skill.java
+++ src/model/Skill.java
@@ -1,1 +1,29 @@
+package model;
 
+public class Skill
+{
+	long skillld;
+	String skillName;
+	
+	public Skill(long skillld, String skillName) {
+		super();
+		this.skillld = skillld;
+		this.skillName = skillName;
+	}
+	
+	public long getSkillld() {
+		return skillld;
+	}
+	public void setSkillld(long skillld) {
+		this.skillld = skillld;
+	}
+	public String getSkillName() {
+		return skillName;
+	}
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+	
+	
+	
+	}
